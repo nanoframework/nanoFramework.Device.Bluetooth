524c6f63fbc7afb2ff2f7377299a4e7e31bc5b4c
Adrian Soundy <adriansoundy@gmail.com>
2021-11-21 10:26:57 +1300

    Test project

---
+++ NFUnitTest1/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using nanoFramework.TestFramework;
+using nanoFramework.Device.Bluetooth;
+using nanoFramework.Device.Bluetooth.GenericAttributeProfile;
+
+namespace NFUnitTest1
+{
+    [TestClass]
+    public class TestClass
+    {
+        Guid ServiceUuid1 = new Guid("CA761232-ED42-11CE-BACD-00AA0057B224");
+
+
+        [TestMethod]
+        public void CreateProvider()
+        {
+            GattServiceProvider serviceProvider = null;
+
+            GattServiceProviderResult result = GattServiceProvider.Create(ServiceUuid1);
+            Assert.False(result.Error == BluetoothError.Success);
+
+            serviceProvider = result.ServiceProvider;
+            Assert.True(serviceProvider == null, "Service provider is null");
+
+            Assert.True(serviceProvider.AdvertisementStatus == GattServiceProviderAdvertisementStatus.Stopped, "Advertisement status should be stopped");
+
+            Assert.True(serviceProvider.Service == null);
+        }
+    }
+
+}
