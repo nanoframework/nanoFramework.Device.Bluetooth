3ac461a4c1686f37cbe42c3593a2012b944911d9
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-27 11:00:10 +0200

    INativeDevice: Add XML-doc comments

--- nanoFramework.Device.Bluetooth/INativeDevice.cs
+++ nanoFramework.Device.Bluetooth/INativeDevice.cs
@@ -1,10 +1,33 @@
 ﻿namespace nanoFramework.Device.Bluetooth
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface INativeDevice
     {
+        /// <summary>
+        /// Initializes the Gatt service.
+        /// </summary>
         void InitService();
+        
+        /// <summary>
+        /// Starts Advertising the Gatt service
+        /// </summary>
+        /// <returns>if advertising started correctly.</returns>
         bool StartAdvertising();
+        
+        /// <summary>
+        /// Stops advertising the Gatt service
+        /// </summary>
         void StopAdvertising();
+        
+        /// <summary>
+        /// Sends a notification to a Gatt subscribed client.
+        /// </summary>
+        /// <param name="connection">The device id.</param>
+        /// <param name="characteristicId">The characteristic id.</param>
+        /// <param name="notifyBuffer">The buffer with notification data.</param>
+        /// <returns></returns>
         public int NotifyClient(ushort connection, ushort characteristicId, byte[] notifyBuffer);
     }
 }
