d2d4c3dcf96fab89f5716dab6987c835a403249f
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-27 11:02:10 +0200

    OnChip: Add XML-doc comments

--- nanoFramework.Device.Bluetooth/NativeDevices/OnChip.cs
+++ nanoFramework.Device.Bluetooth/NativeDevices/OnChip.cs
@@ -2,23 +2,30 @@
 
 namespace nanoFramework.Device.Bluetooth.NativeDevices
 {
+    /// <summary>
+    /// The implementation for devices that use the native call system.
+    /// </summary>
     public class OnChip : INativeDevice
     {
+        /// <inheritdoc />
         public void InitService()
         {
             NativeInitService();
         }
 
+        /// <inheritdoc />
         public bool StartAdvertising()
         {
             return NativeStartAdvertising();
         }
 
+        /// <inheritdoc />
         public void StopAdvertising()
         {
             NativeStopAdvertising();
         }
 
+        /// <inheritdoc />
         public int NotifyClient(ushort connection, ushort characteristicId, byte[] notifyBuffer)
         {
             return NativeNotifyClient(connection, characteristicId, notifyBuffer);
@@ -36,7 +43,7 @@ namespace nanoFramework.Device.Bluetooth.NativeDevices
         private extern void NativeStopAdvertising();
         
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private extern int NativeNotifyClient(ushort connection, ushort CharacteristicId, byte[] notifyBuffer);
+        private extern int NativeNotifyClient(ushort connection, ushort characteristicId, byte[] notifyBuffer);
 
         #endregion
     }
